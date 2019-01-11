8081498e10a39b6287293eb153c152113a8ad749
Dumitru Neacsu <Dumitru.Neacsu@endava.com>
2019-01-11 14:31:21 +0200

    pull

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import jenkins.model.GlobalConfiguration
 
 node {
@@ -10,6 +9,8 @@ node {
               branch: 'master',
               credentialsId: 'git',
               url: 'git@github.com:Mozaic33/Demo.git'
+			  
+	      sh "git pull"
         }
     
         stage ('docker build')
@@ -22,31 +23,4 @@ node {
            sh "docker-compose up -d --remove-orphans"
         }
         
-}
-=======
-import jenkins.model.GlobalConfiguration
-
-node {
-        label 'proiect'
-        ws("/var/lib/jenkins/jobs/DotNET/MyWorkspace"){
-        
-        stage ('Clone repo') {
-          git name: 'Demo',
-              branch: 'master',
-              credentialsId: 'git',
-              url: 'git@github.com:Mozaic33/Demo.git'
-        }
-    
-        stage ('docker build')
-        {
-            sh "docker-compose build aspnetcoreA7"
-        }
-        
-        stage ('docker up')
-        {
-           sh "docker-compose up -d --remove-orphans"
-        }
-        
-}
->>>>>>> a1f0cc2aa7a80867252688887ac4bfa0f07297d4
 }
