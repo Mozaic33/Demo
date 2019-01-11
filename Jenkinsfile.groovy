1beb043ad11f39f994a91f2a4c621a08283efa9f
Mozaic33 <44155625+Mozaic33@users.noreply.github.com>
2019-01-11 12:24:41 +0200

    commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,25 @@
+import jenkins.model.GlobalConfiguration
+
+node {
+        label 'proiect'
+        ws("/var/lib/jenkins/jobs/DotNET/MyWorkspace"){
+        
+        stage ('Clone repo') {
+          git name: 'Demo',
+              branch: 'master',
+              credentialsId: 'git',
+              url: 'git@github.com:Mozaic33/Demo.git'
+        }
+        
+        stage ('docker build')
+        {
+            sh "docker-compose build aspnetcoreA7"
+        }
+        
+        stage ('docker up')
+        {
+           sh "docker-compose up -d --remove-orphans"
+        }
+        
+}
+}
